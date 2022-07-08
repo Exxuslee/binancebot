48e0316992b6112e1e1b939c11a0990e0d286cf3
Exxuslee <exxuslee@gmail.com>
2022-07-08 14:01:44 +0300

    fix quantity

--- src/bot/Bot.ts
+++ src/bot/Bot.ts
@@ -7,7 +7,7 @@ import {Balance} from "./Balance";
 import {View} from "./View";
 import Emittery from "emittery";
 import {Order} from "./Order";
-import {getPricePrecision, getQuantityPrecision} from "../utils/currencyInfo";
+import {getPricePrecision, getQuantityPrecision, validQuantity} from "../utils/currencyInfo";
 import {decimalFloor} from "../utils/math";
 
 export class Bot {
@@ -36,20 +36,18 @@ export class Bot {
         await this.balance.init()
 
         this.strategyConfigs.forEach((strategyConfig) => {
-
             const pair = strategyConfig.asset + strategyConfig.base;
-            let pairBalance: PairBalance = {
-                b1: this.balance.bCurrent(strategyConfig.asset),
-                b2: this.balance.bCurrent(strategyConfig.base),
-                runningBase: this.strategyConfigs.length
-            }
-            log(`Trades ${pair}: ${pairBalance.b1}\t${pairBalance.b2}`);
+            let b1 = this.balance.bCurrent(strategyConfig.asset)
+            let b2 = this.balance.bCurrent(strategyConfig.base)
+            log(`Trades ${pair}: ${b1}\t${b2}`);
             let view = new View(emitter, strategyConfig.leverage)
             let order = new Order()
             order.closeOpenOrders(pair)
             // Precision
             const pricePrecision = getPricePrecision(pair, this.exchangeInfo);
             const quantityPrecision = getQuantityPrecision(pair, this.exchangeInfo);
+            log(`quantityPrecision ${pair}: ${quantityPrecision}`);
+            log(`quantityPrecision ${pair}: ${quantityPrecision}`);
             binanceClient.ws.aggTrades(pair, AggregatedTrade => view.update(AggregatedTrade))
             emitter.on(pair, candlesArray => {
                 this.trade(candlesArray.dataCandles,
@@ -59,32 +57,21 @@ export class Bot {
                     pair,
                     order,
                     candlesArray.currentPrice,
-                    pairBalance,
-                    this.exchangeInfo,
                 )
             })
         })
     }
 
-    async trade(
-        candlesArray, strategyConfig, pricePrecision,
-        quantityPrecision, pair, order,
-        currentPrice, pairBalance, exchangeInfo
-    ) {
+    async trade(candlesArray, strategyConfig, pricePrecision, quantityPrecision, pair, order, currentPrice) {
         if (!order.hasPosition() && strategyConfig.buyStrategy(candlesArray)) {
-            await this.buy(candlesArray, strategyConfig,
-                quantityPrecision, pair, order,
-                currentPrice, pairBalance, exchangeInfo)
+            await this.buy(candlesArray, strategyConfig, pricePrecision, quantityPrecision, pair, order, currentPrice)
         }
         if (!order.hasPosition() && strategyConfig.sellStrategy(candlesArray)) {
             //TODO sell
         }
     }
 
-    async buy(candlesArray, strategyConfig,
-              quantityPrecision, pair, order,
-              currentPrice, pairBalance, exchangeInfo) {
-        const pricePrecision = getPricePrecision(pair, exchangeInfo);
+    async buy(candlesArray, strategyConfig, pricePrecision, quantityPrecision, pair, order, currentPrice) {
         // Calculate TP and SL
         let {takeProfits, stopLoss} = strategyConfig.exitStrategy
             ? strategyConfig.exitStrategy(
@@ -92,7 +79,7 @@ export class Bot {
                 candlesArray,
                 pricePrecision,
                 OrderSide.BUY,
-                exchangeInfo
+                this.exchangeInfo
             )
             : {takeProfits: [], stopLoss: null};
         // Calculate the quantity for the position according to the risk management of the strategy
@@ -100,20 +87,22 @@ export class Bot {
             asset: strategyConfig.asset,
             base: strategyConfig.base,
             balance: strategyConfig.allowPyramiding
-                ? Number(pairBalance.b1)
-                : Number(pairBalance.b2),
+                ? Number(this.balance.bCurrent(strategyConfig.asset))
+                : Number(this.balance.bCurrent(strategyConfig.base)),
             risk: strategyConfig.risk,
             enterPrice: currentPrice,
             stopLossPrice: stopLoss,
-            exchangeInfo: exchangeInfo
+            exchangeInfo: this.exchangeInfo
         });
+        quantity = validQuantity(quantity, pair, this.exchangeInfo)
 
-        log(`${pair} taker buy ${String(quantity)}`);
-        order.newOrder(binanceClient, pair, String(quantity), OrderSide.BUY, OrderType.MARKET).then(() => {
+        log(`${pair} taker buy1 ${String(quantity)}`);
+        log(`${pair} taker buy2 ${String(decimalFloor(quantity, quantityPrecision))}`);
+
+        order.newOrder(binanceClient, pair, String(decimalFloor(quantity, quantityPrecision)), OrderSide.BUY, OrderType.MARKET).then(() => {
             if (takeProfits.length > 0) {
                 // Create the take profit orders
                 takeProfits.forEach(({price, quantityPercentage}) => {
-                    // TODO TAKE_PROFIT_LIMIT
                     log(`${pair} sell maker profit`);
                     order.newOrder(binanceClient, pair, String(decimalFloor(quantity * quantityPercentage, quantityPrecision)), OrderSide.SELL, OrderType.LIMIT, price).catch(error);
                 });
--- src/utils/currencyInfo.ts
+++ src/utils/currencyInfo.ts
@@ -1,19 +1,19 @@
-import { ExchangeInfo } from 'binance-api-node';
-import { decimalCeil } from './math';
+import {ExchangeInfo} from 'binance-api-node';
+import {decimalCeil} from './math';
 import {log} from "./log";
 
 /**
  * @see https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md#lot_size
  */
-export function isValidQuantity(
-  quantity: number,
-  pair: string,
-  exchangeInfo: ExchangeInfo
+export function validQuantity(
+    quantity: number,
+    pair: string,
+    exchangeInfo: ExchangeInfo
 ) {
-  const rules = getLotSizeQuantityRules(pair, exchangeInfo);
-  return (
-    Math.abs(quantity) >= rules.minQty && Math.abs(quantity) <= rules.maxQty
-  );
+    const rules = getLotSizeQuantityRules(pair, exchangeInfo);
+    if (Math.abs(quantity) <= rules.minQty) return rules.minQty
+    else if (Math.abs(quantity) >= rules.minQty) return rules.maxQty
+    else return quantity - quantity % rules.stepSize
 }
 
 /**
@@ -21,14 +21,14 @@ export function isValidQuantity(
  * Binance futures trading rules
  */
 export function getMinOrderQuantity(
-  asset: string,
-  base: string,
-  basePrice: number,
-  exchangeInfo: ExchangeInfo
+    asset: string,
+    base: string,
+    basePrice: number,
+    exchangeInfo: ExchangeInfo
 ) {
-  const precision = getQuantityPrecision(asset + base, exchangeInfo);
-  const minimumNotionalValue = 5; // threshold in USDT
-  return decimalCeil(minimumNotionalValue / basePrice, precision);
+    const precision = getQuantityPrecision(asset + base, exchangeInfo);
+    const minimumNotionalValue = 5; // threshold in USDT
+    return decimalCeil(minimumNotionalValue / basePrice, precision);
 }
 
 /**
@@ -37,55 +37,54 @@ export function getMinOrderQuantity(
  * @see https://www.binance.com/en/support/faq/360033161972
  */
 export function getLotSizeQuantityRules(
-  pair: string,
-  exchangeInfo: ExchangeInfo
+    pair: string,
+    exchangeInfo: ExchangeInfo
 ) {
-  // @ts-ignore
-  const { minQty, maxQty, stepSize } = exchangeInfo.symbols
-    .find((symbol) => symbol.symbol === pair)
     // @ts-ignore
-    .filters.find((filter) => filter.filterType === 'LOT_SIZE');
+    const {minQty, maxQty, stepSize} = exchangeInfo.symbols
+        .find((symbol) => symbol.symbol === pair)
+        // @ts-ignore
+        .filters.find((filter) => filter.filterType === 'LOT_SIZE');
 
-  return {
-    minQty: Number(minQty),
-    maxQty: Number(maxQty),
-    stepSize: Number(stepSize),
-  };
+    return {
+        minQty: Number(minQty),
+        maxQty: Number(maxQty),
+        stepSize: Number(stepSize),
+    };
 }
 
 /**
  * Get the maximal number of decimals for a pair quantity
  */
 export function getQuantityPrecision(pair: string, exchangeInfo: ExchangeInfo) {
-  const symbol = exchangeInfo.symbols.find((symbol) => symbol.symbol === pair);
-  return symbol.quotePrecision;
+    const symbol = exchangeInfo.symbols.find((symbol) => symbol.symbol === pair);
+    return symbol.quotePrecision;
 }
 
 /**
  * Get the maximal number of decimals for a pair quantity
  */
 export function getPricePrecision(pair: string, exchangeInfo: ExchangeInfo) {
-  const symbol = exchangeInfo.symbols.find((symbol) => symbol.symbol === pair);
-  return symbol.baseAssetPrecision;
+    const symbol = exchangeInfo.symbols.find((symbol) => symbol.symbol === pair);
+    return symbol.baseAssetPrecision;
 }
 
 /**
  * Get the tick size for a symbol
  */
 export function getTickSize(pair: string, exchangeInfo: ExchangeInfo) {
-  let tickSize = 0.01000000
-  const symbol = exchangeInfo.symbols.find((symbol) => symbol.symbol === pair);
+    let tickSize = 0.01000000
+    const symbol = exchangeInfo.symbols.find((symbol) => symbol.symbol === pair);
 
-  try {
-    const filter = symbol.filters.find((f) => f.filterType === 'PRICE_FILTER');
-    // @ts-ignore
-    tickSize = Number(filter.tickSize)
-  } catch (e){
-    log(`no tickSize in ${symbol}`)
-  }
-  return tickSize;
+    try {
+        const filter = symbol.filters.find((f) => f.filterType === 'PRICE_FILTER');
+        // @ts-ignore
+        tickSize = Number(filter.tickSize)
+    } catch (e) {
+        log(`no tickSize in ${symbol}`)
+    }
+    return tickSize;
 }
-
 
 
 /**
@@ -97,24 +96,24 @@ export function getCandleSourceType(
     candles: CandleRage[],
     sourceType: SourceType
 ) {
-  switch (sourceType) {
-    case 'open':
-      return candles.map((c) => c.open);
-    case 'high':
-      return candles.map((c) => c.high);
-    case 'low':
-      return candles.map((c) => c.low);
-    case 'close':
-      return candles.map((c) => c.close);
-    case 'hl2':
-      return candles.map((c) => (c.high + c.low) / 2);
-    case 'hlc3':
-      return candles.map((c) => (c.high + c.low + c.close) / 3);
-    case 'hlcc4':
-      return candles.map((c) => (c.high + c.low + c.close * 2) / 4);
-    case 'volume':
-      return candles.map((c) => c.volume);
-    default:
-      return candles.map((c) => c.close);
-  }
+    switch (sourceType) {
+        case 'open':
+            return candles.map((c) => c.open);
+        case 'high':
+            return candles.map((c) => c.high);
+        case 'low':
+            return candles.map((c) => c.low);
+        case 'close':
+            return candles.map((c) => c.close);
+        case 'hl2':
+            return candles.map((c) => (c.high + c.low) / 2);
+        case 'hlc3':
+            return candles.map((c) => (c.high + c.low + c.close) / 3);
+        case 'hlcc4':
+            return candles.map((c) => (c.high + c.low + c.close * 2) / 4);
+        case 'volume':
+            return candles.map((c) => c.volume);
+        default:
+            return candles.map((c) => c.close);
+    }
 }
